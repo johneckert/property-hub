5e954197faa72721f6dcafd9cd28ffd952440933
John Eckert <johnteckert@gmail.com>
2024-08-30 15:05:14 -0400

    fix enum field type

--- db/migrate/20240829144256_create_custom_fields.rb
+++ db/migrate/20240829144256_create_custom_fields.rb
@@ -3,7 +3,7 @@ class CreateCustomFields < ActiveRecord::Migration[7.0]
     create_table :custom_fields do |t|
       t.string :label, null: false
       t.string :internal_name, null: false
-      t.string :field_type, default: 0, limit: 2, null: false
+      t.integer :field_type, default: 0, limit: 2, null: false
       t.belongs_to :client, null: false, foreign_key: { delete: :cascade }
       t.timestamps
     end
